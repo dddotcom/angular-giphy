6acb3e532269db148848f8cb373b1643a6a2a6c1
dddotcom <dddotcom@gmail.com>
2017-03-15 22:10:01 -0700

    committing giphy app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,63 @@
-angular.module('GiphyApp', [])
+var app = angular.module('GiphyApp', ['infinite-scroll', 'ngclipboard']);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+app.controller('GiphyCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
+  $scope.searchTerm = '';
+  $scope.gifs = [];
+  $scope.giphyBusy = false;
+  $scope.after = '';
+  $scope.copiedId = '';
+  $scope.show = false;
+
+  $scope.scrollToTop = function(){
+    $window.scrollTo(0, 0);
+  };
+
+  $scope.onSuccess = function(e) {
+    $scope.copiedId = this.gif.id;
+    console.log($scope.copiedId);
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.clearSelection();
+};
+
+  $scope.search = function(){
+    $scope.gifs = [];
+    var req = {
+      url: 'http://api.giphy.com/v1/gifs/search?',
+      method: 'GET',
+      params: {
+        q: $scope.searchTerm,
+        api_key: 'dc6zaTOxFJmzC'
+      }
+    };
+
+    $http(req).then(function success(res){
+      console.log(res);
+      $scope.gifs = res.data.data;
+    }, function error(res){
+      console.log("error", res);
+    });
+  };
+
+  $scope.giphyNextPage = function(){
+    $scope.giphyBusy = true;
+    var req = {
+      url: 'http://api.giphy.com/v1/gifs/search?',
+      method: 'GET',
+      params: {
+        q: $scope.searchTerm,
+        offset: $scope.gifs.length,
+        api_key: 'dc6zaTOxFJmzC',
+      }
+    };
+
+    $http(req).then(function success(res){
+      console.log(res);
+      var gifs = res.data.data;
+      $scope.gifs = $scope.gifs.concat(gifs);
+      $scope.giphyBusy = false;
+    }.bind($scope));
+  };
+
+}]);
